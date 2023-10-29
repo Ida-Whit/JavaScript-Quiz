9bbbfd93ea5bf1f078698c9dfdacf3b5b7f9b238
Ida Whitcomb <idawhitcomb@gmail.com>
2023-10-29 11:46:13 -0400

    Successsfully created scoring system in JavaScript

--- assets/script.js
+++ assets/script.js
@@ -83,69 +83,123 @@ const prompts = [
 //Declare variables
 let currentQuestion = 0
 let score = 0;
-let timeLeft=90;
+let timeLeft = 90;
 const questionEl = document.getElementById("question-box");
 const startbtn = document.getElementById("start");
 const timeEl = document.getElementById("timer");
-const nextbtn = document.getElementById("next-btn");
+let endScore = document.getElementById("score")
 let questionText = document.getElementById("question");
 let btnA = document.getElementById("btn-a");
 let btnB = document.getElementById("btn-b");
 let btnC = document.getElementById("btn-c");
 let btnD = document.getElementById("btn-d");
-const results = document.getElementById("results")
 let button = document.querySelectorAll(".btn");
+let timer;
+const submitbtn = document.getElementById("submit")
+const highSc = document.getElementById ("scoreScreen")
+let initialInput = document.getElementById ("name")
+let highScore = { initials: initialInput.value,
+    userScore: score
+    };
 
 
-startbtn.addEventListener("click", startQuiz);
 
-
-function startQuiz(){
-    console.log("testing")
+function startQuiz() {
     //Once "start" is clicked, make the button disappear and the quiz questions appear
     startbtn.style.display = "none";
     questionEl.style.display = "block";
     //Pull quiz questions and answer choices from the prompts object and display them on the screen
     //Set a timer countdown to start as soon as the "Start" button is hit.
-    const timer = setInterval(function () {
-    timeLeft--;
-    timeEl.textContent = timeLeft
-    if(timeLeft === 0) {
-        clearInterval(timer);
-    }
-}, 1000);
+    timer = setInterval(function () {
+        timeLeft--;
+        timeEl.textContent = timeLeft
+        if (timeLeft <= 0) {
+            endQuiz()
+
+        }
+    }, 1000);
+    click()
 }
 
 
 
-nextbtn.addEventListener("click", function() {
-    nextQuestion();
+function click() {
     questionText.innerHTML = prompts[currentQuestion].question;
     btnA.innerHTML = prompts[currentQuestion].a;
     btnB.innerHTML = prompts[currentQuestion].b;
     btnC.innerHTML = prompts[currentQuestion].c;
     btnD.innerHTML = prompts[currentQuestion].d;
-});
+}
+
+
 
 function nextQuestion() {
-    for (let i = 0; i < prompts.length; i++) {
-        let userAnswer = prompts[i]
-        if(userAnswer === prompts[i].correctAnswer) {
-        score ++;
+    if (this.textContent !== prompts[currentQuestion].correctAnswer) {
+        timeLeft = timeLeft - 10
+        timeEl.textContent = timeLeft
+    } else {
+        score ++
     }
-    } currentQuestion++
-    if (currentQuestion === prompts.length){
+
+    currentQuestion++
+   
+    if (timeLeft <= 0 || currentQuestion === prompts.length) {
         endQuiz();
+    } else {
+        click()
     }
 };
 
-function endQuiz(){
+function endQuiz() {
     questionEl.style.display = "none";
     results.style.display = "block"
+
+    clearInterval(timer)
+
+    if(timeLeft < 0){
+        timeLeft = 0
+        timeEl.textContent = timeLeft
+    }
+    endScore.innerHTML = score
 }
 
-/*
-btnA.addEventListener("click", )
-btnB.addEventListener("click", )
-btnC.addEventListener("click", )
-btnD.addEventListener("click", )*/
+function scoreList (event) {
+    event.preventDefault();
+    results.style.display = "none"
+    highSc.style.display = "block"
+    localStorage.setItem("highScore", JSON.stringify(highScore));
+}
+
+let header = document.createElement("h2");
+let paragraph = document.createElement("p");
+
+header.textContent = "High Score Board";
+paragraph.innerHTML = JSON.stringify(highScore)
+
+highSc.appendChild(header);
+header.appendChild(paragraph);
+
+//This will be used to get the users initials for the high score board...
+//will need to render the initials into an array before i can do that.
+//const initials = localStorage.getItem ("initials");
+
+
+
+startbtn.addEventListener("click", startQuiz);
+
+btnA.addEventListener("click", nextQuestion)
+btnB.addEventListener("click", nextQuestion)
+btnC.addEventListener("click", nextQuestion)
+btnD.addEventListener("click", nextQuestion)
+
+submitbtn.addEventListener("click", scoreList)
+
+
+/*const email = localStorage.getItem("email");
+const password = localStorage.getItem("password");
+
+<div class="card">
+<h2>Last Registered User:</h2>
+<p><strong>Email:</strong> <span id="user-email"></span></p>
+<p><strong>Password:</strong> <span id="user-password"></span></p>
+</div>*/
